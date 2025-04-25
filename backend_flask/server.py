bfe3ecf7f4d0aca42aba40f1c9c236c964174bfd
Zhou Ji <zhou.ji@yahoo.com>
2025-04-25 10:09:26 -0400

    remove the backend support for the two plots removed

--- backend_flask/server.py
+++ backend_flask/server.py
@@ -15,12 +15,6 @@ mongo = PyMongo(app)
 # Access the "items" collection in the MongoDB database
 trials_collection = mongo.db.aact
 
-# get distinct intervention types
-@app.route('/api/intervention-types', methods=['GET'])
-def get_distinct_intervention_types():
-    intervention_types = trials_collection.distinct("pico_attributes.interventions.type")
-    return jsonify(sorted(intervention_types))
-
 # count trials for all intervention types and outcomes
 @app.route('/api/total', methods=['GET'])
 def total():
@@ -95,42 +89,6 @@ def get_trials_by_intervention_outcome(intervention, outcome):
     for item in counts:
         result.append(item)
     return result
-
-# count trials grouped by conditions
-@app.route('/api/count-by-conditions/<intervention>', methods=['GET'])
-def count_intervention(intervention):
-    counts = trials_collection.aggregate([
-        { "$match": {"pico_attributes.interventions.type": intervention} },
-        # unwind the conditions array (because conditions are stored as arrays)
-        { "$unwind": "$conditions" },
-        # group by condition and count
-        { "$group": { "_id": { "time": "$time", "condition": "$conditions" }, "count": { "$sum": 1 } } },
-        # optional: Sort by count (descending)
-        { "$sort": { "count": -1 } },
-        # reshape the output for readability
-        {
-            "$project": {
-                "year": "$_id.time",
-                "condition": "$_id.condition",
-                "count": 1,
-                "_id": 0  # Exclude the default _id field
-            }
-        }
-    ])
-
-    def rec(result, item):
-        year = item["year"]
-        condition = item["condition"]
-        result["groups"].add(condition)
-        count = item["count"]
-        groups = result["data"]
-        if groups.get(year) is None:
-            groups[year] = {condition: count}
-        else:
-            groups[year][condition] = count
-        return result
-    result = reduce(rec, counts, {"groups": set(), "data":{}})
-    return jsonify(result)
 
 # Route to get distinct conditions
 @app.route('/api/conditions', methods=['GET'])
